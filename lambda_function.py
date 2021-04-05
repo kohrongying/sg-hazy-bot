c4f7fe2334757071fa9049a9c06b07f20aa70668
rongying <kohrongying@gmail.com>
2021-04-05 15:24:44 +0800

    [rongying] Clean up lambda infra.

--- lambda_function.py
+++ lambda_function.py
@@ -2,18 +2,21 @@ import json
 from api_service import main
 
 def lambda_handler(event, context):
-    update = json.loads(event['body'])
+    return main()
+    # update = json.loads(event['body'])
+    #
+    # body = {
+    #     'method': 'sendMessage',
+    #     'chat_id': update['message']['chat']['id'],
+    #     'parse_mode': 'Markdown',
+    #     'text': main(),
+    # }
+    #
+    # response = {
+    #     "statusCode": 200,
+    #     "body": json.dumps(body)
+    # }
+    #
+    # return response
 
-    body = {
-        'method': 'sendMessage',
-        'chat_id': update['message']['chat']['id'],
-        'parse_mode': 'Markdown',
-        'text': main(),
-    }
-
-    response = {
-        "statusCode": 200,
-        "body": json.dumps(body)
-    }
-
-    return response
+print(lambda_handler({},{}))
--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -5,15 +5,19 @@ locals {
 resource "aws_lambda_function" "this" {
   function_name = local.lambda_function_name
   role          = aws_iam_role.iam_for_lambda.arn
-  runtime = "python3.8"
+  runtime       = "python3.8"
   handler       = "lambda_function.lambda_handler"
-  filename = "index.zip"
+  filename      = "index.zip"
   source_code_hash = filebase64sha256("index.zip")
 
   environment {
     variables = {
       foo = "bar"
     }
+  }
+
+  lifecycle {
+    ignore_changes = [source_code_hash]
   }
 }
 
