a742cc2aeb8ec8ecfe1e0ea16a7b3a0895bfe1b1
rongying <kohrongying@gmail.com>
2021-04-05 23:44:26 +0800

    [rongying] Update bot markdown.

--- lambda_function.py
+++ lambda_function.py
@@ -1,22 +1,16 @@
 import json
 from api_service import main
 
+
 def lambda_handler(event, context):
     update = json.loads(event['body'])
 
     body = {
         'method': 'sendMessage',
         'chat_id': update['message']['chat']['id'],
-        'parse_mode': 'Markdown',
+        'parse_mode': 'HTML',
         'text': main(),
     }
-
-    response = {
-        "statusCode": 200,
-        "body": json.dumps(body)
-    }
-
-    return response
 
     response = {
         "statusCode": 200,
